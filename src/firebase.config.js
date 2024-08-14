941faea1eb7b0c7a5d8ba8ebe5218bc98d048130
vahagn <vahag.kostanyan974@gmail.com>
2024-08-14 11:23:12 +0400

    add env and read configs from it

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider, GithubAuthProvider, } from "firebase/auth"
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-PFrJoM6e69pz8fR1mFUGmA8m-ja8ZbA",
-  authDomain: "passtspace.firebaseapp.com",
-  projectId: "passtspace",
-  storageBucket: "passtspace.appspot.com",
-  messagingSenderId: "297741052660",
-  appId: "1:297741052660:web:54886e8523c8e627564ca1",
-  measurementId: "G-T314BNGSQJ"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
